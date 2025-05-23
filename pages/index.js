eb590b14218db107ac756583bbeb58b7bb1c04eb
T8020Token <anthony@t8020consulting.com>
2025-05-23 05:25:09 +0000

    Add new button for different Crossmint organization

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Home from '../crossmint-integration/Home';
+
+export default function Index() {
+    return (
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+            <h1>CJ Art Of Trading Membership Token</h1>
+            <Home />
+        </div>
+    );
+}
